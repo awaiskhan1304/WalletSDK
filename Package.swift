84bd09fd09d4b1ab5c43ba28c24f62054029a6bf
Awais Khan <awais.cs10@gmail.com>
2023-07-28 23:15:01 +0500

    added back binary target in package.json

--- Package.swift
+++ Package.swift
@@ -21,22 +21,15 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-//        .binaryTarget(
-//            name: "WalletKitFramework",
-//            path: "./Sources/WalletKitFramework.xcframework"),
-//        .target(
-//            name: "Dependencies-Wrapper",
-//            dependencies: [
-//                .target(name: "WalletKitFramework"),
-//                "web3swift"
-//            ]
-//        )
-        .target(
+        .binaryTarget(
             name: "WalletKitFramework",
+            path: "./Sources/WalletKitFramework.xcframework"),
+        .target(
+            name: "Dependencies-Wrapper",
             dependencies: [
+                .target(name: "WalletKitFramework"),
                 "web3swift"
-            ],
-            path: "./Sources/Dependencies-Wrapper"
+            ]
         )
     ]
 )
